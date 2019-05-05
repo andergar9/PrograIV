5daf4e3db862caaed3aa81b60d732cc0d59897df
andersong2 <andergarcia2499@gmail.com>
2019-05-05 10:31:23 -0500

    Completar

--- Turnero/src/Modelo/Person.java
+++ Turnero/src/Modelo/Person.java
@@ -1,5 +1,30 @@
 package Modelo;
 
 public class Person {
+	private String codigo;
+    private String turno;
+	
+	public Person(String codigo, String turno) {
+		this.codigo = codigo;
+		this.turno = turno;
+	}
+	
+	
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getTurno() {
+		return turno;
+	}
+
+	public void setTurno(String turno) {
+		this.turno = turno;
+	}
+
 
 }
--- Turnero/src/Modelo/Usuario.java
+++ Turnero/src/Modelo/Usuario.java
@@ -1,5 +1,39 @@
 package Modelo;
 
-public class Usuario {
+public class Usuario extends Persona {
+	private String id;
+	private String servicio;
+	private String priori;
+	
+	public Usuario(String id, String name, String servicio, String priori) {
+		super(name);
+		this.id = id;
+		this.servicio = servicio;
+		this.priori = priori;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getServicio() {
+		return servicio;
+	}
+
+	public void setServicio(String servicio) {
+		this.servicio = servicio;
+	}
+
+	public String getPriori() {
+		return priori;
+	}
+
+	public void setPriori(String priori) {
+		this.priori = priori;
+	}
 
 }

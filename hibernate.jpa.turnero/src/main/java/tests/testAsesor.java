fe9fffa206266360eb1f67ec07d4d400a08407e7
andersong2 <andergarcia2499@gmail.com>
2019-05-17 15:55:41 -0500

    Actualizo ORM clase Asesor

--- hibernate.jpa.turnero/src/main/java/tests/testAsesor.java
+++ hibernate.jpa.turnero/src/main/java/tests/testAsesor.java
@@ -1,5 +1,6 @@
 package tests;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -8,18 +9,38 @@ import co.nihil.turnero.modelo.Asesor;
 
 
 
+
 public class testAsesor {
 	
 	
 	private static EntityManager manager;
 	private static EntityManagerFactory emf;
+	@SuppressWarnings("unused")
+	private static ArrayList<String> servicio,servicio1,servicio2,servicio3;
 
 	public static void main(String[] args) {
 	/*Crear gestor de persistencia */	
 	emf = Persistence.createEntityManagerFactory("Persistencia");
 	manager = emf.createEntityManager();
+	Asesor a = new Asesor("Juan", servicio2, 1, 1);
 	
-
+	manager.getTransaction().begin();
+	manager.persist(a);
+	manager.getTransaction().commit();
+	
+	 imprimir();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static void imprimir() {
+		List <Asesor> ase = (List<Asesor>)manager.createQuery("FROM Asesores").getResultList();
+		System.out.println("Hay"+ ase.size() +"Asesores en el sistema");
+		for(Asesor us:ase) {
+			System.out.println(us.toString());
 	}
 
+	
+	}
 }
+	
+
